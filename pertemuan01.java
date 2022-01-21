7e933a07d3f1e75c302e43f1b515b45970b09fc3
masikdev <79369996+masikdev@users.noreply.github.com>
2022-01-21 09:20:47 +0700

    Add files via upload

---
+++ pertemuan01.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class pertemuan01 {
+    public static void main(String[] args) {
+        // PERCOBAAN DILAKUKAN 1
+        System.out.println("Percobaan Dilakukan 1");
+        System.out.println("Nama          : Muhammad Rizky Baskara");
+        System.out.println("Gender        : Laki - laki");
+        System.out.println("Alamat        : Genteng, Banyuwangi, Jawa Timur");
+        System.out.println("Tanggal Lahir : Banyuwangi, 03 November 2001");
+
+        // PERCOBAAN DILAKUKAN 2
+        System.out.println("Percobaan Dilakukan 2");
+        System.out.println("Nama Buah:");
+        System.out.println("1. Manggis");
+        System.out.println("2. Semangka");
+        System.out.println("3. Durian");
+        System.out.println("4. Salak");
+
+        System.out.println("Nama Buah:\b");
+        System.out.println("1. Manggis\b");
+        System.out.println("2. Semangka\b");
+        System.out.println("3. Durian\b");
+        System.out.println("4. Salak\b");
+
+        System.out.println("Nama Buah:\n");
+        System.out.println("1. Manggis\n");
+        System.out.println("2. Semangka\n");
+        System.out.println("3. Durian\n");
+        System.out.println("4. Salak\n");
+
+        System.out.println("\tNama Buah:");
+        System.out.println("\t1. Manggis");
+        System.out.println("\t2. Semangka");
+        System.out.println("\t3. Durian");
+        System.out.println("\t4. Salak");
+
+    }
+
+}
